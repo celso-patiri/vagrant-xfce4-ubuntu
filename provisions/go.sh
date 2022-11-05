fce7ac863422b17bee6af3e8ef68c8f6c1f367d8
Celso Patiri <celsobenedetti2@gmail.com>
2022-11-04 21:14:46 -0300

    new: lazygit + gotop

--- provisions/go.sh
+++ provisions/go.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# install go
 curl -OL https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
-
 sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+rm ~/go*.tar.gz
+
+# install lazygit
+/usr/local/go/bin/go install github.com/jesseduffield/lazygit@latest
+
+# install gotop
+/usr/local/go/bin/go install github.com/xxxserxxx/gotop/v4/cmd/gotop@latest
